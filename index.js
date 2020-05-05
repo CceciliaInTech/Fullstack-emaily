e5fa10d66ad970d63c21a4a2c6002227556816ee
xi li <li.xi1@husky.neu.edu>
2020-05-04 20:21:16 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+// on the server side, we use commonJS to import, unlike front=end using ES26
+const express = require('express');
+//generate a new express app, to set up configuration to listen for rounte handlers
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send({ hi: 'there' });
+});
+
+// PRODUCTION will use whatever PORT heroku tells
+const PORT = process.env.PORT || 5000;
+app.listen(5000);
