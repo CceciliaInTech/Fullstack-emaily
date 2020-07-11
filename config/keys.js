b559a1ea3b8d163556cecba5eef8fef80bb94c65
xi li <li.xi1@husky.neu.edu>
2020-07-10 23:13:14 -0700

    force https

--- config/keys.js
+++ config/keys.js
@@ -1,6 +1,20 @@
 // keys.js  production side
 if (process.env.NODE_ENV === 'production') {
+  app.use((req, res, next) => {
+    if (req.header('x-forwarded-proto') !== 'https')
+      res.redirect(`https://${req.header('host')}${req.url}`);
+    else next();
+  });
   module.exports = require('./prod');
 } else {
   module.exports = require('./dev');
 }
+
+// if(process.env.NODE_ENV === 'production') {
+//   app.use((req, res, next) => {
+//     if (req.header('x-forwarded-proto') !== 'https')
+//       res.redirect(`https://${req.header('host')}${req.url}`)
+//     else
+//       next()
+//   })
+// }
