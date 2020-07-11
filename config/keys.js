7b1cce8a9c06f3f69d5cc15cb665f8e66820322a
xi li <li.xi1@husky.neu.edu>
2020-07-11 10:10:05 -0700

    dev

--- config/keys.js
+++ config/keys.js
@@ -1,14 +1,14 @@
 // keys.js  production side
-if (process.env.NODE_ENV === 'production') {
-  app.use((req, res, next) => {
-    if (req.header('x-forwarded-proto') !== 'https')
-      res.redirect(`https://${req.header('host')}${req.url}`);
-    else next();
-  });
-  module.exports = require('./prod');
-} else {
-  module.exports = require('./dev');
-}
+// if (process.env.NODE_ENV === 'production') {
+//   app.use((req, res, next) => {
+//     if (req.header('x-forwarded-proto') !== 'https')
+//       res.redirect(`https://${req.header('host')}${req.url}`);
+//     else next();
+//   });
+//   module.exports = require('./prod');
+// } else {
+//   module.exports = require('./dev');
+// }
 
 // if(process.env.NODE_ENV === 'production') {
 //   app.use((req, res, next) => {
@@ -18,3 +18,11 @@ if (process.env.NODE_ENV === 'production') {
 //       next()
 //   })
 // }
+
+if (process.env.NODE_ENV === 'production') {
+  //return production keys
+  module.exports = require('./prod');
+} else {
+  // return development keys
+  module.exports = require('./dev');
+}
